1ecc81db05c66fb7e6776c97cf2ded84aee068fe
Ronald-tech <ronieqtsang@gmail.com>
2024-02-27 10:46:53 -0800

    1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -2,3 +2,4 @@
 # regular expression that will match the above cases
 # Ruby script that accepts one argument and pass it to a regular expression matching method
 
+puts ARGV[0].scan(/hbt{2,5}n/).join
