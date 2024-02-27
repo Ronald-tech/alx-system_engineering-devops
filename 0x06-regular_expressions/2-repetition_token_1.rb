abd0a99387b760393337958dfd1d9849dd3f4485
Ronald-tech <ronieqtsang@gmail.com>
2024-02-27 10:57:46 -0800

    2-repetition_token_1.rb

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -2,4 +2,4 @@
 # regular expression that will match the above cases
 # Ruby script that accepts one argument and pass it to a regular expression matching method
 
-puts ARGV[0].scan(hb?tn/).join
+puts ARGV[0].scan(/hb?tn/).join
