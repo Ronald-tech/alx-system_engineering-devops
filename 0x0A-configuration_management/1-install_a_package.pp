ac108b9732408b4510ca9daf4361e8814daa4716
Ronald-tech <ronieqtsang@gmail.com>
2024-04-19 13:30:00 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# install puppet-lint
+# Using Puppet, install flask from pip3
 
-package {'flask':
-ensure   => '2.1.0',
-provider => 'pip3'
+package { 'flask':
+ ensure   => '2.1.0',
+ provider => 'pip3'
 }
